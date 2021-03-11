e08cdede9a70b27bfd94c7e9b803106ef6ed5e6d
YourSergic1 <yoursergic1@gmail.com>
2021-03-12 00:52:14 +0300

    gulp
    fixed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,10 @@ function bs() {
   watch("./*.html").on('change', browserSync.reload);
   watch("./js/*.js").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass); 
+  watch("./sass/**/*.scss", serveSass); 
 };
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
       .pipe(sass())
       .pipe(autoprefixer({
         cascade: false
