c855b68023af56f234aa911d9f5671b24c8bbfd3
YourSergic1 <yoursergic1@gmail.com>
2021-03-24 19:09:51 +0300

    deleted send.php

--- js/main.js
+++ js/main.js
@@ -94,8 +94,9 @@ $(document).ready(function () {
       messages:{
         userName: "Имя обязатеьно!",
         userPhone: "Телефон обязателен!",
-      }
+      },
     });
+    
 
 
     $('[type=tel]').mask('+7(000) 000-00-00' , {placeholder: "+7 (___) ___-__-__"});
